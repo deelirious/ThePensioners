8854ab3cb027e7e17374c55393548b1fcc844615
Nadia Klimenko <nmk2502@gmail.com>
2018-03-19 13:57:13 +0100

    added parameters in the supervisor part

--- Someren/Someren/SomerenDB.cs
+++ Someren/Someren/SomerenDB.cs
@@ -366,22 +366,15 @@ namespace Someren
         public static void DB_removeSupervisor(string remove_supervisor)
         {
             SqlConnection connection = openConnectionDB();
-            // List<SomerenModel.Supervisor> supervisors = new List<SomerenModel.Supervisor>();
-
-            //put the query into string builder
-            StringBuilder sb = new StringBuilder();
-            sb.Append("UPDATE Teachers SET IsSupervisor = '0'WHERE LastName like '" + remove_supervisor + "'");
-
-            String sql = sb.ToString();
+            
+            String sql = @"UPDATE Teachers SET [IsSupervisor] = '0' WHERE convert(varchar,LastName) = @LastName";
 
             //start connection with DB
             SqlCommand command = new SqlCommand(sql, connection);
-            SqlDataReader reader = command.ExecuteReader();
-            while (reader.Read())
-            {
+            
+                command.Parameters.AddWithValue("@LastName", remove_supervisor);
                 command.ExecuteNonQuery();
-            }
-            reader.Close();
+            
 
             connection.Close();
         }
@@ -389,22 +382,17 @@ namespace Someren
         public static void DB_AddSupervisor(string add_supervisor)
         {
             SqlConnection connection = openConnectionDB();
-            // List<SomerenModel.Supervisor> supervisors = new List<SomerenModel.Supervisor>();
 
-            //put the query into string builder
-            StringBuilder sb = new StringBuilder();
-            sb.Append("UPDATE Teachers SET IsSupervisor = '1'WHERE LastName like '" + add_supervisor + "'");
 
-            String sql = sb.ToString();
+
+            String sql = @"UPDATE Teachers SET [IsSupervisor] = '1' WHERE convert(varchar, LastName) =  @LastName";
 
             //start connection with DB
             SqlCommand command = new SqlCommand(sql, connection);
-            SqlDataReader reader = command.ExecuteReader();
-            while (reader.Read())
-            {
+            
+                command.Parameters.AddWithValue("@LastName", add_supervisor);
                 command.ExecuteNonQuery();
-            }
-            reader.Close();
+           
 
             connection.Close();
         }
